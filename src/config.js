41633cec2cfa54dfeacd4e7b4ff5b62d0d655432
Tomas <tomasholderness@gmail.com>
2018-07-12 14:58:48 -0400

    initial Lambda

--- src/config.js
+++ src/config.js
@@ -4,5 +4,12 @@ require('dotenv').config({silent: true});
 export default {
     DETIK_URL: process.env.DETIK_URL,
     HISTORICA_LOAD_PERIOD: process.env.HISTORICAL_LOAD_PERIOD || 3600000,
+    PGUSER: process.env.PGUSER || 'postgres',
+    PGPASSWORD: process.env.PGPASSWORD,
+    PGHOST: process.env.PGHOST || 'localhost',
+    PGPORT: process.env.PGPORT || 5432,
+    PGDATABASE: process.env.PGDATABASE || 'cognicity',
+    PGSSL: process.env.PGSSL === 'true' || false,
+    PG_CLIENT_IDLE_TIMEOUT: process.env.PG_CLIENT_IDLE_TIMEOUT || 100,
     TABLE_DETIK: process.env.TABLE_DETIK || 'detik.reports',
 };
---
+++ src/functions/detik/index.js
@@ -0,0 +1,31 @@
+import {Pool} from 'pg'; // Postgres
+import config from '../../config';
+import DetikDataSource from ('../lib/detik');
+
+// Connection object
+const cn = `postgres://${config.PGUSER}:${config.PGPASSWORD}@${config.PGHOST}:${config.PGPORT}/${config.PGDATABASE}?ssl=${config.PGSSL}`;
+
+// Create a pool object
+const pool = new Pool({
+  connectionString: cn,
+  idleTimeoutMillis: config.PG_CLIENT_IDLE_TIMEOUT,
+});
+
+// Endpoint for detik polling lambda
+export default async (event, context, callback) => {
+    try {
+            // Catch database errors
+            pool.on('error', (err, client) => {
+                console.error('Unexpected error on idle client', err);
+            });
+
+            // Create instance
+            const detikDataSource = new DetikDataSource(config, pool);
+
+            // Start polling
+            detikDataSource.start();
+
+    } catch (err) {
+        console.log('Error running Detik poll.', err.message)
+    }
+}
